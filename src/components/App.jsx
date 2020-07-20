946eaeb28438d7e8cc31fe1d2031c3ad9f659abe
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-20 11:54:31 -0700

    sccafolding

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../index.css';
+import Header from "./Header";
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <FPControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
