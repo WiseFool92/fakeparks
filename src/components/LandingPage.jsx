468c14fd477721031d3b2bc1c62c66637b8030ad
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-21 15:57:31 -0700

    Continued CSS

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,16 +3,23 @@ import React from "react";
 function LandingPage(){
   return (
     <>
+  
       <h1 className="homeDrop">Welcome To Le Parks</h1>
       
       <h2>About Our Parks</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>The National Park Service preserves unimpaired the natural and cultural resources and values of the National Park System for the enjoyment, education, and inspiration of this and future generations. The Park Service cooperates with partners to extend the benefits of natural and cultural resource conservation and outdoor recreation throughout this country and the world.</p>
 
       <h2> Park Rules </h2>
+      <h4>Acceptable</h4>
         <ul className="rules">
-          <li>Biking, skating & rollerblades are okay</li>
-          <li>No alcohol</li>
-          <li>No fireworks</li>
+          <li>Rollerblades</li>
+          <li>Skating</li>
+          <li>Biking</li>
+        </ul>
+      <h4>Unacceptable</h4>
+        <ul className="rules">
+          <li>Fireworks</li>
+          <li>Alcohol</li>
         </ul>
     </>
   );
